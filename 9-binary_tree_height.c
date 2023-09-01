0800c376b1b502e18f2c497f2729f45952cc7f42
maggywairigu <wairigumaina2016@gmail.com>
2023-09-01 09:28:32 -0700

    file 9 added

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * @tree: tree to go through
  * Return: the height
  */
-size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l = 0;
 	size_t r = 0;
@@ -18,7 +18,7 @@ size_t binary_tree_height(const binary_tree_t *tree);
 		if (tree)
 		{
 			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right 1 + binary_tree_height(tree->right) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
 		return ((l > r) ? l : r);
 	}
